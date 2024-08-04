051f7a07590b4d7bfdac51810a877d38be32db2e
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-08-04 23:49:57 +0700

    add nimate

--- components/header/dashboard/MenuHeader.tsx
+++ components/header/dashboard/MenuHeader.tsx
@@ -1,4 +1,6 @@
 import {
+  Animated,
+  Easing,
   Pressable,
   SafeAreaView,
   StyleSheet,
@@ -10,21 +12,64 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import EvilIcons from "@expo/vector-icons/EvilIcons";
 import { useRouter } from "expo-router";
+import { BG_ORANGE_600 } from "@/constants/colors";
+import { useCallback, useEffect, useRef } from "react";
 
 const MenuHeader = () => {
   const router = useRouter();
+  const animatedValue1 = useRef(new Animated.Value(0)).current;
+
+  const animLoopRef1 = useRef<any>();
+
+  const triggerAnimation = useCallback(() => {
+    animLoopRef1.current = Animated.loop(
+      Animated.timing(animatedValue1, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: false,
+      })
+    );
+    animLoopRef1.current.start();
+  }, [animatedValue1]);
+
+  useEffect(() => {
+    triggerAnimation();
+  }, []);
+
+  const turnToTop = (animate: any) => {
+    return {
+      translateY: animate.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, -30],
+      }),
+    };
+  };
+
+  const turnOpacity = (animate: any) => {
+    return animate.interpolate({
+      inputRange: [0, 0.7],
+      outputRange: [1, 0],
+    });
+  };
+
+  const animation1 = {
+    transform: [
+      turnToTop(animatedValue1),
+    ],
+    opacity: turnOpacity(animatedValue1),
+  };
+
   const navigateToChatPage = () => {
-    router.push("/conversation")
-  }
+    router.push("/conversation");
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <EvilIcons name="search" size={24} color="black" />
-        <TextInput
-          style={styles.input}
-          placeholder="Đồ ngủ cho chó"
-          placeholderTextColor="red"
-        />
+        <View style={styles.input} />
+        <Animated.Text style={[styles.animateValue1,animation1]}>
+          Quần xịp lọt khe
+        </Animated.Text>
         <EvilIcons name="camera" size={24} color="black" />
       </View>
 
@@ -66,6 +111,7 @@ const styles = StyleSheet.create({
     gap: 10,
     alignItems: "center",
     justifyContent: "center",
+  
     // backgroundColor: 'transparent'
   },
   inputContainer: {
@@ -75,6 +121,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "white",
     width: "70%",
+    zIndex: 1
   },
   input: {
     width: "75%",
@@ -101,5 +148,14 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: "white",
+  },
+  animateValue1: {
+    position: "absolute",
+    top: 10,
+    left: "16%",
+    zIndex: 1,
+    color: BG_ORANGE_600,
+    fontSize: 15,
+    fontWeight: "600",
   },
 });
